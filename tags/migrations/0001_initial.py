51d2913130fbd6a6c14ddbb9fdd6accd2672eceb
matheuscas <matheus.mcas@gmail.com>
2022-06-21 21:38:27 -0300

    chore: run pre-commit hooks on the project

--- tags/migrations/0001_initial.py
+++ tags/migrations/0001_initial.py
@@ -7,16 +7,23 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Tag',
+            name="Tag",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=150)),
-                ('color', models.CharField(max_length=100)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=150)),
+                ("color", models.CharField(max_length=100)),
             ],
         ),
     ]
