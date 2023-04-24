35aa36503c2632778d6f7e12082531dbeb749527
matheuscas <matheus.mcas@gmail.com>
2023-04-23 22:03:09 -0300

    fix: change a typo

--- use_cases/records/exceptions.py
+++ use_cases/records/exceptions.py
@@ -1,6 +1,6 @@
 class RecordMissingIdException(Exception):
-    msg = "records's id is missing. Can' update a records without it"
+    msg = "record's id is missing. Can' update a records without it"
 
 
 class RecordNotFoundException(Exception):
-    msg = "records's not found"
+    msg = "record's not found"
