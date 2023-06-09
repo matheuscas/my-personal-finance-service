64c3cba47dfbad39bd2e4f3ec951d41d2d71f379
matheuscas <matheus.mcas@gmail.com>
2023-06-09 18:07:11 -0300

    feat: create UserRepository concrete implementation (create method only)

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.db import IntegrityError, models
 from django.utils.translation import gettext_lazy as _
 
+from .entities import User
 from .managers import CustomUserManager
+from .use_cases.exceptions import ExistingUserException
 
 
 class CustomUser(AbstractUser):
@@ -20,3 +22,29 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class UserRepository:
+    def create(self, user: User) -> User:
+        custom_user = {
+            "first_name": user.name,
+            "email": user.email,
+            "password": user.password,
+        }
+        try:
+            created_user = CustomUser.objects.create_user(**custom_user)
+        except IntegrityError as e:
+            raise ExistingUserException() from e
+
+        return User(
+            name=created_user.first_name,
+            email=created_user.email,
+            password=created_user.password,
+            id=created_user.id,
+        )
+
+    def update(self, id: str, user: User) -> User:
+        ...
+
+    def get(self, id: str) -> User:
+        ...
