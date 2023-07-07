118187cba9eb1bae5d756b3373e0cfb2d922ea9d
matheuscas <matheus.mcas@gmail.com>
2023-07-07 17:27:47 -0300

    feat: add openapi dynamic generation
    Still need customization

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,8 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.api.urls")),
+    path(
+        "openapi/",
+        get_schema_view(
+            title="Once again, one more personal finance service",
+            description="API docs",
+            version="0.0.1",
+        ),
+        name="openapi-schema",
+    ),
+    path(
+        "",
+        TemplateView.as_view(
+            template_name="swagger-ui.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="swagger-ui",
+    ),
 ]
